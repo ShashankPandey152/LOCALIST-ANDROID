e831460ceaa0a2ad506fe0e7901b1939540b354f
frankhart2017 <sdharchou@gmail.com>
2018-06-03 20:27:23 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# LOCALIST-ANDROID" 
---
+++ app/src/main/java/com/android/siddhartha/localist/settings/Constants.java
@@ -0,0 +1,7 @@
+package com.android.siddhartha.localist.settings;
+
+public class Constants {
+
+    public static final int LOCATION_INTERVAL = 10000;
+    public static final int FASTEST_LOCATION_INTERVAL = 5000;
+}
